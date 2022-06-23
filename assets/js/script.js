dcbf647ae5fdbd945d542f9c2c5816e643df8f4e
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-06-23 11:28:55 +0000

    Nothing to log, but need to save and take a break

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,52 @@
 
-document.addEventListener("DOMContentLoaded", function() {
-var buttonBox = document.getElementById("button-box");
+document.addEventListener("DOMContentLoaded", startScreen());
+var gameHasStarted = false;
 
-for (let startBtn = document.createElement("button")){
-    startBtn.setAttribute('onclick', 'startTheGame()');
+
+/* Test story*/
+const myStory = [{
+    "first" : "It was dark and lonely night",
+    "second" : "This too is a sentence"
+}]
+
+
+/* Need to figure out here how to remove start button after StartTheGame function has started*/
+function startScreen() {
+    let buttonBox = document.getElementById("button-box");
+    let startBtn = document.createElement("button");
+    buttonBox.appendChild(startBtn);
     startBtn.innerHTML = "Start the story";
-    buttonBox.appendChild(startBtn);  
+    console.log("Hi from startBtn");
+    startBtn.addEventListener('click', startTheGame)
+    if (gameHasStarted === true) {
+        buttonBox.removeChild(startBtn);
+    }
+
 }
 
-
-})
-
-
 function startTheGame() {
+    gameHasStarted = true;
+    console.log("Game has started");
     let buttonBox = document.getElementById("button-box");
-    let startBtn = document.getElementsByName("startBtn");
+    let button = buttonBox[0];
+    console.log(button);
+    buttonBox.removeChildren();
     let btn1 = document.createElement("button");
     let btn2 = document.createElement("button");
     btn1.innerHTML = "This way";
     btn2.innerHTML = "That way";
     buttonBox.appendChild(btn1);
     buttonBox.appendChild(btn2);
+    console.log(buttonBox.children);
 
-    var storyBox = document.getElementById("story");
+    let gameScreen = document.getElementById("game-screen");
+    let storyBox = document.createElement('div');
+    gameScreen.appendChild(storyBox);
+    let text = document.getElementsByName(myStory);
 
-    /** From here the story pages should go to json-file */
-let page1 = document.createElement("div");
-page1.innerHTML = "You are dead. Very, very dead."
+    console.log(text.first);
+}
+
+function changePage(){
+
 }
