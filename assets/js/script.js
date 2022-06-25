8115e989a2821824d3cf09d14db66ed41410d390
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-06-25 09:33:11 +0000

    Add: Start button from JS to HTML, were able to hide it now

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,32 +5,26 @@ var gameHasStarted = false;
 
 /* Test story*/
 const myStory = [{
-    "first" : "It was dark and lonely night",
-    "second" : "This too is a sentence"
+    first : ["It was dark and lonely night" , "Go forward" , "Go backward"],
+    second : ["This too is a sentence" , "Go forward" , "Go backward"]
 }]
 
 
 /* Need to figure out here how to remove start button after StartTheGame function has started*/
 function startScreen() {
-    let buttonBox = document.getElementById("button-box");
-    let startBtn = document.createElement("button");
-    buttonBox.appendChild(startBtn);
+    let startBtn = document.getElementById("start-button");
     startBtn.innerHTML = "Start the story";
     console.log("Hi from startBtn");
-    startBtn.addEventListener('click', startTheGame)
-    if (gameHasStarted === true) {
-        buttonBox.removeChild(startBtn);
-    }
-
+    startBtn.addEventListener('click', startTheGame);
 }
 
 function startTheGame() {
     gameHasStarted = true;
+    let startBtn = document.getElementById("start-button");
+    startBtn.classList.add("hidden");
     console.log("Game has started");
     let buttonBox = document.getElementById("button-box");
-    let button = buttonBox[0];
-    console.log(button);
-    buttonBox.removeChildren();
+    console.log("buttonBox");
     let btn1 = document.createElement("button");
     let btn2 = document.createElement("button");
     btn1.innerHTML = "This way";
