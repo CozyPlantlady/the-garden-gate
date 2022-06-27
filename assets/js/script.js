54c17aa9a4f7c14883bb00e40a175b74f03829df
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-06-27 08:11:43 +0000

    Add: make code for changing text for buttons and story page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,20 @@
 
 document.addEventListener("DOMContentLoaded", startScreen());
 var gameHasStarted = false;
+let buttonBox = document.getElementById("button-box");
 
 
 /* Test story*/
 const myStory = [{
-    first : ["It was dark and lonely night" , "Go forward" , "Go backward"],
-    second : ["This too is a sentence" , "Go forward" , "Go backward"]
+    page0 : "And so our story begins",
+    page1 : "It was dark and lonely night",
+    page2 : "This too is a sentence"
 }]
-
+const myButtons = [{
+    option0 : "Nothing here",
+    option1 : "Go fish",
+    option2 : "Go home"
+}]
 
 /* Need to figure out here how to remove start button after StartTheGame function has started*/
 function startScreen() {
@@ -17,30 +23,64 @@ function startScreen() {
     console.log("Hi from startBtn");
     startBtn.addEventListener('click', startTheGame);
 }
-
+/**This function uses the other functions to start the game. 
+ * It also hides the start button */
 function startTheGame() {
     gameHasStarted = true;
     let startBtn = document.getElementById("start-button");
     startBtn.classList.add("hidden");
     console.log("Game has started");
-    let buttonBox = document.getElementById("button-box");
-    console.log("buttonBox");
+    btnOneText();
+    btnTwoText();
+    storyBox();
+}
+
+
+
+function btnOneText(){
+    const myButtons = [{
+        option0 : "Nothing here",
+        option1 : "Go fish",
+        option2 : "Go home"
+    }]
     let btn1 = document.createElement("button");
-    let btn2 = document.createElement("button");
-    btn1.innerHTML = "This way";
-    btn2.innerHTML = "That way";
+    let btn1NextText = myButtons.option0;
+    btn1.innertext = btn1NextText;
     buttonBox.appendChild(btn1);
+    console.log(buttonBox.children);
+}
+function btnTwoText(){
+    let btn2 = document.createElement("button");
+    btn2.innerHTML = "That way";
     buttonBox.appendChild(btn2);
     console.log(buttonBox.children);
+}
 
+ function storyBox(){
     let gameScreen = document.getElementById("game-screen");
-    let storyBox = document.createElement('div');
+    let storyBox = document.getElementById("story");
     gameScreen.appendChild(storyBox);
-    let text = document.getElementsByName(myStory);
+    let text = document.getElementsByName("myStory");
+    document.getElementById('story');
+    text = myStory.page1.innerText;
 
-    console.log(text.first);
+    console.log(myStory.page1);
 }
 
 function changePage(){
+
+    let page;
+    let pageNumber = 0;/* What user chooses , fix that*/
+    switch (pageNumber) {
+        case 0:
+            page = document.getElementsByName("myStory").innerText;
+            break;
+
+        case 1:
+            page = document.getElementsByTagName("myStory").innerText;
+        default: 
+        page = "This didn't work. It's not you, it's the code."
+            break;
+    }
 
 }
