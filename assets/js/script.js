4dc6971aac927776730dc8626c451466b5127c6a
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-07-04 13:29:41 +0000

    Add: The final ending

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,9 @@ const nameBox = document.querySelector('#name-div');
 let playerName = document.querySelector('#player-name').value;
 console.log(playerName);
 let murderWeapon;
+let familiarFace;
+let labyrinthSecret;
+let trueEnding;
 
 
 /*options for button 1 */
@@ -56,7 +59,7 @@ const myStory = {
     page8 : `When you enter the labyrith you remember being there before. You have been playing hide and seek in there so may times it's not scary at all. It's overgrown, but it doesnt't bother your ghostly form. Someone else has not been as lucky you think, as you follow the tracks of broken twigs, torn pieces of clothing and what you now suspect to be a faint trail of blood to the center of the labyrinth.`,
     page9 : `As you arrive to the center of the labyrinth you are surprised to see that you are not there alone. First, you see a candle light. Then, a person that is sitting on the ground. He looks disheveled, dirty, and seems to be chanting something with their eyes closed. You know this person too. It's your cousin, Drew. You used to play here together as kids.`,
     page10 : `A chill goes through your body, again. You are sure that this shovel has been used to kill you. Unfortunately, there doesn't seem to be anything else to see here. At that moment you see first sunrays shining through the dirty window, and your form slowly disappears from the sight. Morning has come.`,
-    page11 : `You go to the gate, and touch it's surface. As you push your hand through the gate you feel uncomfortable tingling and then get violently pushed back in the garden. You try it couple of times, and same happens again. Something is holding you here, and you cant't leave before you know what it is. You still wait by the garden gate when the morning sun rises, and you disappear from the sight.`,
+    page11 : `You go to the gate, and touch it's surface. As you push your hand through the gate you feel uncomfortable tingling.`,
     page12 : `You look the area around the gate closer. In distance you can see the manor this garden belongs to. Everything around the gate is overgrown, except the door of the gate that seems to be in frequent use. You see fresh footprints on the ground that seem to be going towards a labyrinth. The padlock on the gate is open, and hanging from the door.`,
     page13 : `You feel uncomfortable staying under the oak, and leave right away. Your way continues to the garden gate, maybe you can get out from here.`,
     page14 : `There are letters laying on the ground. They have been stomped over, and you get the feeling that there have been a struggle.You read whats on the top most letter, and recognise your own handwriting:“Dear aunt Agatha,I'm sorry to hear that your health is declining. I'll be sure to come see you as soon as I can!From ${playerName}”You see tracks going towards the pond.`,
@@ -64,10 +67,12 @@ const myStory = {
     page16 : `You approach the misty figure. As you get closer, you recognize the familiar face.“Dear ${playerName}, is it really you? You finally came!” she speaks with voice that could be a whisper.It's your aunt, Agatha, who is the owner of the manor this garden belongs to. You have spend so many summers here, playing in the garden and the labyrinth. Oh how you missed those days.`,
     page17 : `You get so terrified that you simply just run away. You keep going until morning comes and your form fades away from sight.`,
     page18 : `As you inspect closer you see that these are all the letters you wrote to your aunt Agatha over the years. They start from when you were just a child and learning to write, full of scribbles of things you had seen. Agatha has saved each and everyone.You are lost in memories as the sun climbs up.`,
-    page19 : `As you wake up in the gazebo once again it all clicks.You had been summoned by aunt Agatha to come see her since she had fallen ill. When you arrived you were met by cousin Drew instead, who told you that Agatha had gone to the garden, and that you could find her in the gazebo.  Drew said that he would tag along. When you got there, Drew hit your head with a garden shovel, killing you. Then he dragged your body to the middle of the labyrinth, burying you to the shallow grave.But cousin Drew was superstitious, and had always been afraid of ghosts. That made him make some sort of spell to make sure your spirit, as well as Agathas, would stay in the garden...`,
+    page19 : `You push your hand through the cold metal, and all the memories come back to you .You had been summoned by aunt Agatha to come see her since she had fallen ill, but when you arrived you were met by cousin Drew instead. He told you that Agatha had gone to the garden, and that you could find her in the gazebo.  Drew said that he would tag along. When you got there, Drew hit your head with a garden shovel, killing you.`,
     page20 : `As you arrive to the garden gate, you hear You hear Agathas voice: ”You know ${playerName}, all I wanted was to give the manor to you. It is yours, if you ask me. I think we should go and pay Drew a visit. “You are finally able to leave the garden, and head through the gate. It's still dark, moon shines over the garden and the road leading towards the manor. It's time for the master of the house to arrive home.The end.`,
     page21 : `You feel the cold chill going through you and know for sure, that he is the one that killed you. And here, center of the labyrinth, is where he dragged your body and buried you. Drew keeps chanting and you feel your surrounding go black. He is trying to keep your spirit in the garden. You remember him always being so afraid of ghosts... You wish to give him something to be afraid of, but before you can, your form fades as you see the sun rising.`,
     page22 : `“I came here to read your letters, but your cousin Drew followed me. He drowned me. I have been waiting for you to come.”Agatha grabs your hand, and you feel peace flowing through you. At least you are not alone here.You dont mind the sun coming up, and you feel warmth as you both disappear from the sight.`,
+    page23 : `You touch the gate and get violently pushed back to the garden. You try it couple of times, and same happens again. Something is holding you here, and you cant't leave before you know what it is. You still wait by the garden gate when the morning sun rises, and you disappear from the sight.`,
+    page24 : `Then he dragged your body to the middle of the labyrinth, burying you to the shallow grave. But cousin Drew was superstitious, and had always been afraid of ghosts. That made him make some sort of spell to make sure your spirit, as well as Agathas, would stay in the garden...`,
 };  
 /**This function holds everything else inside of it. This is hopefully giving some structure. */
 function gameFile(){
@@ -181,6 +186,11 @@ function gameFile(){
                 btn1.textContent = "";
                 btn2.textContent = ""; 
                 pageNumber = 11;
+                if (TrueEnding === true){
+                    btn1.textContent = btn1NextText.option5;
+                    btn2.textContent = ""; 
+                    pageNumber = 19;
+                }
                 break;
             case 6:
                 btn1.textContent = "";
@@ -202,6 +212,11 @@ function gameFile(){
                 btn2.textContent = "";
                 pageNumber = 21;
                 break;
+            case 11:
+                btn1.textcontent = myButtons2.option11;
+                btn2.textContent = "";
+                pageNumber = 19;
+                break;
             case 14:
                 btn1.textContent = btn1NextText.option8;
                 btn2.textContent = btn2NextText.option9; 
@@ -211,6 +226,11 @@ function gameFile(){
                 btn1.textContent = btn1NextText.option10;
                 btn2.textContent = btn2NextText.option10; 
                 pageNumber = 17;
+                break;
+            case 19:
+                btn1.textContent = "";
+                btn2.textContent = ""; 
+                pageNumber = 20;
                 break;
             default:
                 btn1.textContent = "There has been an error. Please refresh the page";
@@ -252,6 +272,7 @@ function gameFile(){
                 btn1.textContent = "";
                 btn2.textContent = btn2NextText.option5; 
                 pageNumber = 12;
+                break;
             case 6:
                 btn1.textContent = btn1NextText.option8;
                 btn2.textContent = btn2NextText.option9; 
@@ -370,16 +391,14 @@ function gameFile(){
                 storyBox.innerHTML = myStory.page20;
                 theEnd();
                 break;
-            case 20:
-                storyBox.innerHTML = myStory.page20;
-                theEnd();
-                break;
             case 21:
                 storyBox.innerHTML = myStory.page21;
+                labyrinthSecret = true;
                 theEnd();
                 break;
             case 22:
                 storyBox.innerHTML = myStory.page22;
+                familiarFace = true;
                 theEnd();
                 break;
             
@@ -400,6 +419,17 @@ function gameFile(){
     startBtn.classList.add("visible")
     if (murderWeapon === true){
         alert("You discovered the murder weapon!");
+    }
+    if (familiarFace === true){
+        alert("You found the reason you came here!")
+    }
+    if (labyrinthSecret === true){
+        alert("You found the murderer!")
+    }
+    if (murderWeapon && familiarFace && labyrinthSecret === true){
+        TrueEnding = true;
+        alert("You found the secret ending! Head to the garden gate!")
+        pageNumber = 19;
     }
     ++replay;
     replayTest(replay);
